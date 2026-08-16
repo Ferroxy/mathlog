6fec687cf6dd8be9775a4cd356cf1fc0fd6629d9
Ferhat TEKKOUK <38411423+Ferroxy@users.noreply.github.com>
2026-08-16 19:46:53 +0100

    Refactor image background and visibility handling
    Fix visibility toggling and update background image logic.

--- script.js
+++ script.js
@@ -13,22 +13,22 @@ fnd.addEventListener('click', () => {
     const fond = document.querySelector(':root')
     const rep = vAleat % 3
     switch (rep % 3) {
-        case 2:
-            fond.style.setProperty('--imgFond', "url('soldier.png')")
-            reper.forEach(element => {
-                element.style.setProperty('visibility', 'visible')
-            });
-            vAleat++
-            break;
-        case 1:
-            fond.style.setProperty('--imgFond', "url('racines.png')")
-            vAleat++
-            break;
         case 0:
             fond.style.setProperty('--imgFond', "url('')")
             reper.forEach(element => {
                 element.style.setProperty('visibility', 'hidden')
             });
+            vAleat++
+            break;
+        case 1:
+            fond.style.setProperty('--imgFond', "url('racines.png')")
+            reper.forEach(element => {
+                element.style.setProperty('visibility', 'visible')
+            });
+            vAleat++
+            break;
+        case 2:
+            fond.style.setProperty('--imgFond', "url('soldier.png')")
             vAleat++
             break;
     }
